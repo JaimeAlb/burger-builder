037febf685ef802798f66cfa4791e1d1dd352691
JaimeAlb <jaimealbornoz2001@hotmail.com>
2021-01-14 14:44:44 -0300

    render Burger statically

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import Aux1 from '../../hoc/Aux1';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
     render() {
         return (
             <Aux1>
-                <div>Burger</div>
+                <Burger />
                 <div>Build Controls</div>
             </Aux1>
         );
