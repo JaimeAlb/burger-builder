fe2d4167d7a7281c1dc2d4b1ec53b988ac174fa8
JaimeAlb <jaimealbornoz2001@hotmail.com>
2021-01-17 11:52:27 -0300

    create state for BurgerBuilder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,11 +2,24 @@ import React, { Component } from "react";
 import Aux1 from '../../hoc/Aux1';
 import Burger from '../../components/Burger/Burger';
 
+// stateful component
 class BurgerBuilder extends Component {
+    // constructor(props){  // old fashion 
+    //     super(props);
+    //     this.state = {...}
+    // }
+    state = {               //modern fashion
+        ingredients: { //the goal is to pass this object to the burger. We can't use map on it, it's not an array
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
     render() {
         return (
             <Aux1>
-                <Burger />
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
             </Aux1>
         );
