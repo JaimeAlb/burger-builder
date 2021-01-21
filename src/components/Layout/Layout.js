7a0cf46b6823a7c88c3a475a2ef807f435b4f630
JaimeAlb <jaimealbornoz2001@hotmail.com>
2021-01-21 12:38:10 -0300

    none

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,12 +3,12 @@ import Aux1 from '../../hoc/Aux1';
 import classes from './Layout.css';
 
 const layout = (props) => (
-        <Aux1>
+    <Aux1>
         <div>Toolbar, SideDrawer, Backdrop</div>
         <main className={classes.Content}>
             {props.children}
         </main>
-        </Aux1>
+    </Aux1>
 );
 
 export default layout;
